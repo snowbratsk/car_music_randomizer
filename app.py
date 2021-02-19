b719aaf2b28bbe5bf108c2b9828ab540098097ff
snowbratsk <snow.bratsk@gmail.com>
2021-02-19 18:57:45 +0800

    added logic for get_params()

--- app.py
+++ app.py
@@ -1,10 +1,62 @@
+import sys, os
+
+
 def get_params():
-    pass
+    if len(sys.argv) == 4:
+        directory = sys.argv[1]
+        flash = sys.argv[1]
+        limit = int(sys.argv[1])
+    else:
+        if (
+            input(
+                "You didn'o't enter required parameters. Want to enter them now? [Y / n]:"
+            )
+            == "n"
+        ):
+            sys.exit()
+        else:
+            while True:
+                directory = input(
+                    "Please, enter directory with your music, or 'quit' to quit:"
+                )
+                if os.path.exists(directory):
+                    print("Directory {} accepted.".format(directory))
+                    break
+                elif directory.lower() == "quit":
+                    sys.exit()
+                else:
+                    print("Directory {} does not exist!".format(directory))
+
+            while True:
+                flash = (
+                    input("Please, enter flash disk letter or 'quit' to quit:") + ":\\"
+                )
+                if os.path.exists(flash):
+                    print("Flash disk on {} accepted.".format(flash))
+                    break
+                elif flash.lower() == "quit":
+                    sys.exit()
+                else:
+                    print("Flash disk on {} not found!".format(flash))
+
+            while True:
+                limit = input("Please, enter amount of files or 'quit' to quit:")
+                if limit.isdigit():
+                    print("{} file limit accepted.".format(limit))
+                    limit = int(limit)
+                    break
+                elif limit.lower() == "quit":
+                    sys.exit()
+                else:
+                    print("Incorrect number {}!".format(limit))
+    return directory, flash, limit
 
 
-def main():
-    pass
+def main(args):
+    directory, flash, limit = args
+    print(args)
+    print(directory, flash, limit)
 
 
 if __name__ == "__main__":
-    main()
+    main(get_params())
